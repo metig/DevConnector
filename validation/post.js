36dd12146a4afafcb26fb6f03bbe7dca0d3fff9c
meti <49797027+metig@users.noreply.github.com>
2019-07-01 12:52:16 -0700

    post chages

--- validation/post.js
+++ validation/post.js
@@ -1,5 +1,4 @@
 const Validator = require('validator');
-
 const isEmpty = require('./is-empty');
 
 
@@ -7,34 +6,21 @@ const isEmpty = require('./is-empty');
 module.exports = function validatePostInput(data) {
 
   let errors = {};
-
-
-
   data.text = !isEmpty(data.text) ? data.text : '';
 
-
-
-  if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
-
-    errors.text = 'Post must be between 10 and 300 characters';
+    if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
+       errors.text = 'Post must be between 10 and 300 characters';
 
   }
 
-
-
-  if (Validator.isEmpty(data.text)) {
-
-    errors.text = 'Text field is required';
+    if (Validator.isEmpty(data.text)) {
+       errors.text = 'Text field is required';
 
   }
 
-
-
-  return {
-
-    errors,
-
-    isValid: isEmpty(errors)
+    return {
+      errors,
+      isValid: isEmpty(errors)
 
   };
 
